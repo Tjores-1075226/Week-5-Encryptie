d5bae5e13d90fdc45f0082060aa37a019150396f
Tjores-1075226 <1075226@hr.nl>
2024-10-13 22:21:16 +0200

    enc for test and dec for tekst

--- enc/dec.py
+++ enc/dec.py
@@ -10,3 +10,20 @@ def generate_key():
 # sleutel laden
 def load_key():
     return open("secret.key", "rb").read()
+
+#tekst versleutelen
+def encrypt_message(message):
+    key = load_key()
+    fernet = Fernet(key)
+    encrypted_message = fernet.encrypt(message.encode())
+    return encrypted_message
+
+
+# tekst ontsleutelen
+def decrypt_message(encrypted_message):
+    key = load_key()
+    fernet = Fernet(key)
+    decrypted_message = fernet.decrypt(encrypted_message).decode()
+    return decrypted_message
+
+
